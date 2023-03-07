d82ace0f8ff0827d5ae84f9c686ce576353a7351
mrDilmurat <dilmurat.ug@gmail.com>
2023-03-07 16:43:40 +0600

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,20 +38,20 @@ const Footer = () => {
                             <h4 className="text-white mb-4">Contact Us</h4>
                             <div>
                                 <address className="text-white fs-6">
-                                    Hno : 277 Near Vill chopal, <br /> Sonipat, Haryana <br />
-                                    PinCode: 131103
+                                    Kyrgyzstan Bishkek <br /> 7 microdistrict <br />
+                                    home 116a/17
                                 </address>
                                 <a
                                     href="tel:+91 8264954234"
                                     className="mt-3 d-block mb-1 text-white"
                                 >
-                                    +91 8264954234
+                                    +996558755756
                                 </a>
                                 <a
                                     href="mailto:navdeepdahiya753@gmail.com"
                                     className="mt-2 d-block mb-0 text-white"
                                 >
-                                    navdeepdahiya753@gmail.com
+                                    hello.dilmurat@gmail.com
                                 </a>
                                 <div className="social_icons d-flex align-items-center gap-30 mt-4">
                                     <a className="text-white" href="#">
